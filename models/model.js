8f93774d7cda728bbb7272164010f0f223dd1e49
Beni Samuel <benisamuel566@gmail.com>
2024-05-05 14:00:34 +0200

    adding the models

---
+++ models/model.js
@@ -0,0 +1,18 @@
+const mongoose=required("mongoose");
+
+//creating the schema for the our collection
+const courseSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        minlength:4,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    }
+})
+
+const Course=mongoose.model("Course",courseSchema)
+
+module.exports = Course;
